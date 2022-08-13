1705f9867ac6a3fabc94604cd6172cd8fcbd4b23
A.Bakr <75008256+ahmedhbakr@users.noreply.github.com>
2022-08-13 12:37:37 +0200

    Create __manifest__.py

---
+++ 11.0/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Force Delete Multiple | Invoice/Credit Note',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'sequence': 14,
+    'summary': 'Invoicing, Number, Cancelled,delete',
+    'author': 'Ahmed hassan',
+    'website': "#",
+    'version': '0.1',
+    'images': [
+    ],
+    'depends': [
+        'account_cancel'
+    ],
+    'data': [
+        'wizards/invoice_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
